f9a46f395d51c1fd82edbef61f9783eb382d25ea
Mriganka Gogoi <gogoimriganka09@gmail.com>
2022-07-19 23:25:21 +0530

    Traingle Pattern in java

--- TrainglePattern2.java
+++ TrainglePattern2.java
@@ -1,21 +1,22 @@
 import java.util.*;
-public class Pattern1 {
-
+public class TrainglePattern2 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
         int i=1;
+
         while(i<=n){
             int j=1;
-            while(j<=n){
-                System.out.print("*");
+            while(j<=i){
+                System.out.print(j);
                 j=j+1;
             }
             System.out.println();
             i=i+1;
+
+
         }
-
-
+        
     }
+    
 }
